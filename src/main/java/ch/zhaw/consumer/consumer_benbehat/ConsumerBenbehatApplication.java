6e966d5a0b271bb186f7713c2517430f36b722f2
Hatem Ben Belgacem <hatem.benbelgacem@outlook.com>
2026-05-05 23:09:58 +0200

    first commit

---
+++ src/main/java/ch/zhaw/consumer/consumer_benbehat/ConsumerBenbehatApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.consumer.consumer_benbehat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerBenbehatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerBenbehatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/consumer/consumer_benbehat/ConsumerBenbehatApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.consumer.consumer_benbehat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerBenbehatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
